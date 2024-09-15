9b2e0f606be61e102eef9f9d240009fc62012062
Sensie21 <mafaaz.sabir@gmail.com>
2024-09-15 19:16:02 +0530

    Changed language

---
+++ rag.py
@@ -0,0 +1,25 @@
+import params
+from pymongo import MongoClient
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+import warnings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpoint
+import json
+
+warnings.filterwarnings("ignore", category=UserWarning, module="langchain.chains.llm")
+
+client = MongoClient(params.mongodb_conn_string)
+collection = client[params.db_name][params.collection_name]
+
+vectorStore = MongoDBAtlasVectorSearch(
+    collection, HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2"), index_name=params.index_name
+)
+
+docs = vectorStore.max_marginal_relevance_search('add product photos, numbers, price', k=1)
+
+for doc in docs:
+    file_path = doc.metadata['source']
+    with open(file_path,'r') as file:
+        data = json.loads(file.read())
+        print(data['intent'])
+    
